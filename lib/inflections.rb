ce5d8d1bae67066ab7d65b62ac383ff77417b7d3
Ethan Waldo <ewaldo@talho.org>
2011-04-18 08:30:15 -0500

    Inflections are conflicting with clearance somehow.  Fortunately this inflection isn't actually needed.

--- lib/inflections.rb
+++ lib/inflections.rb
@@ -1,3 +1,3 @@
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.irregular('HanAlert', 'HanAlerts')
-end
+#ActiveSupport::Inflector.inflections do |inflect|
+#  inflect.irregular('HanAlert', 'HanAlerts')
+#end
