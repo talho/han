d2b2708204e7ac75b5187220c41545836eb05d7d
Charles DuBose <cdubose@ubuntu.(none)>
2011-08-09 08:25:11 -0500

    Fixing install script to not bail

--- install.rb
+++ install.rb
@@ -3,12 +3,12 @@ parent_lib_dir = File.join(Rails.root, "lib")
 [ "workers" ].each { |lib_subdir|
   rel_path = File.join("..","..","vendor","plugins","han","lib",lib_subdir)
   target = File.join(parent_lib_dir, lib_subdir, "han")
-  File.symlink(rel_path, target) unless File.exists?(target)
+  File.symlink(rel_path, target) unless File.symlink?(target)
 }
 
 rel_path = File.join("..","..","..","..","spec","spec_helper.rb")
 target = "#{Rails.root}/vendor/plugins/han/spec/spec_helper.rb"
-File.symlink(rel_path, target) unless File.exists?(target)
+File.symlink(rel_path, target) unless File.symlink?(target)
 
 # Create links in Rails.root/public so that the register_javascript_expansion()
 # and register_stylesheet_expansion() methods can see the plugin's files.
@@ -16,5 +16,5 @@ parent_public_dir = File.join(Rails.root, "public")
 [ "javascripts", "stylesheets" ].each { |public_subdir|
   rel_path = File.join("..","..","vendor","plugins","han","public",public_subdir)
   target = File.join(parent_public_dir, public_subdir, "han")
-  File.symlink(rel_path, target) unless File.exists?(target)
+  File.symlink(rel_path, target) unless File.symlink?(target)
 }
