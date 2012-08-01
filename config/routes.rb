ff181e67f0a1d799901b73b2b2f2b95360107214
Richard Boldway <rboldway@talho.org>
2012-08-01 09:27:10 -0500

    Added :show and :update to han_alerts resource for the Log and Reporting admin page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 
 Openphin::Application.routes.draw do
-  resources :han_alerts, :only => [:index,:create,:new,:edit] do
+  resources :han_alerts, :only => [:index,:create,:new,:edit,:show,:update] do
       collection do
         get :recent
       end
   end
   match "/han_alerts/index/upload" => "han_alerts#upload", :as => :upload, :method => [:get, :post]
   match "/han_alerts/new/playback.wav" => "han_alerts#playback", :as => :playback, :method => [:get]
-  match "/han_alerts/calculate_recipient_count.:format", :to => "han_alerts#calculate_recipient_count"
+  match "/han_alerts/calculate_recipient_count.:format" => "han_alerts#calculate_recipient_count"
 end
