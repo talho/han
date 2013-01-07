295ba20685a965b5afde0cbcb2ea79cb425dc381
cdubose <cdubose@marlin.(none)>
2013-01-07 12:09:40 -0600

    removing old han recipe

--- features/step_definitions/jurisdiction_steps.rb
+++ features/step_definitions/jurisdiction_steps.rb
@@ -6,7 +6,7 @@ end
 Then /^I should not see "(.*)" as a from jurisdiction option$/ do |name|
   jurisdiction = Jurisdiction.find_by_name!(name)
   begin
-    wait_until do
+    waiter do
       begin
         page.find("select#han_alert_from_jurisdiction_id option[@value=\"#{jurisdiction.id.to_s}\"]").nil?
         assert false
